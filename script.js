430bce07de4414495cbb9a5211c0bfc616a427e0
danokirror@gmail.com <danokirror@gmail.com>
2024-05-03 00:51:58 +0300

    written the first suedo js code for the password checker

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-let m ="";
+const firstPassword = document.getElementById('Password');
+const finalPassword = document.getElementById('Confirm-Password');
+let display = document.getElementById('Password-Checker');
+let displayMessage1 = "Please enter the Password!";
+let displayMessage2 = "Please confirm the Password!";
+let displayMessage3 = "Password not matching!!!!";
+let displayMessage4 = "Password Matching";
+
+
+function checkPassword() {
+    let password1 = firstPassword.nodeValue;
+    let password2 = finalPassword.nodeValue;
+
+    if(password1 == '') {
+        display.innerHTML = displayMessage1;
+    }
+    else if(password2 == '') {
+        display.innerHTML = displayMessage2;
+    }
+    else if(password1 != password2) {
+        display.innerHTML = displayMessage3;
+    }
+    else {
+        display.innerHTML = displayMessage4;
+    }
+}
+
+firstPassword.addEventListener('click',checkPassword);
+finalPassword.addEventListener('click',checkPassword);
